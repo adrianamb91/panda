754a3a54e6e814a47732e6ccc484d61bd515625d
Adriana <adriana.bogza@gmail.com>
2013-11-27 21:03:52 +0200

    Tried to persist object: didn't succeed!

--- backend/src/com/timesheetapplication/servlets/LoginServlet.java
+++ backend/src/com/timesheetapplication/servlets/LoginServlet.java
@@ -2,19 +2,31 @@ package com.timesheetapplication.servlets;
 
 import java.io.IOException;
 
+import javax.persistence.MappedSuperclass;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.timesheetapplication.dao.GenericDao;
+import com.timesheetapplication.daoImpl.GenericDaoImpl;
+import com.timesheetapplication.model.Clerk;
+import com.timesheetapplication.model.Employee;
+
 /**
  * Servlet implementation class LoginServlet
  */
 @WebServlet("/LoginServlet")
+@RequestMapping("/Login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    
+	//GenericDao<Clerk> dao = new GenericDaoImpl<Clerk>();
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -34,6 +46,14 @@ public class LoginServlet extends HttpServlet {
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		System.out.println("Username: " + username + "\nPassword: " + password);
+		
+		//Employee e = new Clerk();
+		//e.setFirstName(username);
+		//e.setLastName(username);
+		
+		//dao.saveOrUpdate(e);
+	
+		//System.out.println("presumably persisted");
 	}
 
 	/**
